168a50e475e70f5f995f3ff9d13c6051976e4496
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-03 23:46:14 +0530

    Update main.tf

--- infra/modules/api_gateway/main.tf
+++ infra/modules/api_gateway/main.tf
@@ -66,6 +66,29 @@ resource "aws_api_gateway_deployment" "this" {
     aws_api_gateway_method.this         # Make sure method is created first
   ]
 }
+
+resource "aws_iam_role" "api_gateway_cloudwatch_role" {
+  name = "api_gateway_cloudwatch_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_cloudwatch_attach" {
+  role       = aws_iam_role.api_gateway_cloudwatch_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
 resource "aws_api_gateway_account" "account" {
-  cloudwatch_role_arn = var.api_gateway_cloudwatch_role_arn
+  cloudwatch_role_arn = aws_iam_role.api_gateway_cloudwatch_role.arn
 }
