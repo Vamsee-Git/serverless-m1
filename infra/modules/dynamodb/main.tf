47482119b672fd818f130611fea18460532d06d8
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-03 14:52:11 +0530

    Update main.tf

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table" "this" {
   name           = var.table_name
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "userid"
+  hash_key       = "_id"
 
   attribute {
-    name = "userid"
+    name = "_id"
     type = "S"
   }
 }
